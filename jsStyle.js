60cd9a6eaa81eca399f34cb9a7b498c474045858
Guy Hennequin <guy.a.hennequin@gmail.com>
2023-04-19 22:40:26 +0900

    Update html and jsStyle file with button elements and classes with correlating styling

--- jsStyle.js
+++ jsStyle.js
@@ -15,7 +15,7 @@ calcBodyClass.split(" ").forEach((cls) => calcBody.classList.add(cls));
 
 // output class
 const outputClass = 
-    "flex flex-col 2-3/4 h-12 justify-center items-end m-1 bg-gray-300 text-gray-900 font-semibold px-2 border border-black hover:border-gray-500 rounded";
+    "flex flex-col w-3/4 h-12 justify-center items-end m-1 bg-gray-300 text-gray-900 font-semibold px-2 border border-black hover:border-gray-500 rounded";
 const output = document.querySelector(".output");
 outputClass.split(" ").forEach((cls) => output.classList.add(cls)); 
 
@@ -32,6 +32,34 @@ const currentNumber = document.querySelector(".currentNumber");
 currentNumberClass.split(" ").forEach((cls) => currentNumber.classList.add(cls)); 
 
 // all buttons
+const buttons = document.querySelectorAll('button');
+buttons.forEach((btn) => {
+    addClasses(btn)
+})
 
+// specific button types
+function addClasses(button) {
+const btnNumberClass = "hover:bg-green-500 border-green-500";
+const btnOperatorClass = "hover:bg-blue-500 border-blue-500";
+const btnClearClass = "hover:bg-orange-500 border-orange-500";
+const btnDecimalClass = "hover:bg-red-500 border-red-500";
+const btnEqualsClass = "hover:bg-yellow-500 border-yellow-500";
 
+if (button.classList.value === "number") {
+    btnNumberClass.split(" ").forEach((cls) => button.classList.add(cls));
+} else if (button.classList.value === "operator") {
+    btnOperatorClass.split(" ").forEach((cls) => button.classList.add(cls));
+} else if (button.classList.value === "clear") {
+    btnClearClass.split(" ").forEach((cls) => button.classList.add(cls));
+} else if (button.classList.value === "decimal") {
+    btnDecimalClass.split(" ").forEach((cls) => button.classList.add(cls));
+} else if (button.classList.value === "equals") {
+    btnEqualsClass.split(" ").forEach((cls) => button.classList.add(cls));
+}
+
+// all buttons
+const baseClass = 
+    "number flex w-12 h-12 justify-center items-center m-1 font-bold py-2 px-2 border hover:border-transparent rounded text-white border"
+    baseClass.split(" ").forEach((cls) => button.classList.add(cls));
+}
 
