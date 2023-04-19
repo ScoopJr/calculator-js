5767a5bf18d7b412bf60e743e0ffb110cc2441b7
Guy Hennequin <guy.a.hennequin@gmail.com>
2023-04-19 22:43:51 +0900

    Update js style file to update baseClass button to bold

--- jsStyle.js
+++ jsStyle.js
@@ -59,7 +59,7 @@ if (button.classList.value === "number") {
 
 // all buttons
 const baseClass = 
-    "number flex w-12 h-12 justify-center items-center m-1 font-bold py-2 px-2 border hover:border-transparent rounded text-white border"
+    "number flex w-12 h-12 justify-center items-center m-1 font-bold py-2 px-2 border hover:border-transparent rounded text-white border-2 text-2xl"
     baseClass.split(" ").forEach((cls) => button.classList.add(cls));
 }
 
